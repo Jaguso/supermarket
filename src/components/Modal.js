3deb899ea2a491977d0637800e95c1aa3523aa2d
Jaguso <jagutzo@gmail.com>
2019-04-17 22:16:47 -0500

    used flexbox

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,9 +8,13 @@ class Modal extends Component {
   render() {
     return  ReactDOM.createPortal(
       <div className="ui dinner modals visible active" id="main-div">
-        <div className="ui standard modal visible active">
-          <h1>this is sparta</h1>
-          <p>sdoifjsodifj</p>
+        <div className="ui standard modal visible active" id="inner-div">
+          <h2>Add item</h2>
+          <input/>
+          <div >
+            <button>Cancel</button>
+            <button>Add</button>
+          </div>
         </div>
       </div>,
       document.querySelector('#modal')
