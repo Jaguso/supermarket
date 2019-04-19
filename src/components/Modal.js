85a0556b0994e4ee7514158777053d2979306cc4
Jaguso <jagutzo@gmail.com>
2019-04-18 21:26:01 -0500

    input in modal changes local storage

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,16 +5,45 @@ import './Modal.css';
 
 
 class Modal extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      itemname: ""
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target
+
+    console.log("Valores: ", name, value);
+    this.setState({[name]: value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('item: ', this.state.itemname);
+  }
+
   render() {
     return  ReactDOM.createPortal(
       <div className="ui dinner modals visible active" id="main-div">
         <div className="ui standard modal visible active" id="inner-div">
           <h2 className="modal-title">Add items</h2>
-          <input className="input" />
-          <div className="buttons">
-            <button id="button" className="button-1"><a href="/">Cancel</a></button>
-            <button id="button" className="button-2">Add</button>
-          </div>
+          <form onSubmit={this.onSubmit}>
+            <input 
+              className="input" 
+              type="text" 
+              name="itemname" 
+              value={this.state.itemname} 
+              onChange={this.onChangeInput} 
+              />
+            <div className="buttons">
+              <button id="button" className="button-1"><a href="/">Cancel</a></button>
+              <button id="button" className="button-2" type="submit">Add</button>
+            </div>
+          </form>
+            
         </div>
       </div>,
       document.querySelector('#modal')
