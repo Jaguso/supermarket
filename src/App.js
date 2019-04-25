9be617603a5524b8849acd5c65b8853b36b863de
Jaguso <jagutzo@gmail.com>
2019-04-24 20:26:58 -0500

    added loading simulation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import ListItems from  './components/ListItems';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { isLoading: true }
+  }
+
+  componentDidMount() {
+    setTimeout(function() {
+      this.setState({isLoading: false})
+    }.bind(this), 1500)
+  }
+
   render() {
     return (
+      this.state.isLoading ? 
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <h4>Loading...</h4>
+      </div>
+      : 
       <div className="App">
         <ListItems/>
       </div>
